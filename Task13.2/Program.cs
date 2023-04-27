ce29c4a486c1ef1d013cde00a63666f966217131
Dina Nevhod <Dina_Nevhod@epam.com>
2023-04-27 11:03:29 +0200

    Add project files.

---
+++ Task13.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task13._2
+{
+    internal class Program
+    {
+        static void MyMethod1()
+        {
+            Console.WriteLine("MyMethod1 запущено");
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine("*");
+            }
+            Console.WriteLine("MyMethod1 завершено");
+        }
+        static void MyMethod2()
+        {
+            Console.WriteLine("MyMethod2 запущено");
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine("+");
+            }
+            Console.WriteLine("MyMethod2 завершено");
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Метод Main запущено");
+
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.MaxDegreeOfParallelism = 2;
+
+            Parallel.Invoke(parallelOptions,MyMethod1,MyMethod2);
+            Console.WriteLine("Метод Main завершено");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task13.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task13._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[1000000];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1,100);
+            }
+
+            ParallelQuery<int> numbers = from nums in array.AsParallel()
+                                         where nums%2!=0
+                                         select nums;
+            foreach (var n in numbers)
+            {
+                Console.Write(n + ", ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
